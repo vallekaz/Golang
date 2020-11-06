15f71519611e075f0734a11115f335d1b32a756a
jantome <59940015+jantome@users.noreply.github.com>
2020-11-06 21:50:09 +0100

    Create structs.go

---
+++ jantome/cumplehoras/structs/structs.go
@@ -0,0 +1,11 @@
+package structs
+
+//Ejecucion struct de la tabla de ejecucion para recuperar el nombre
+type Ejecucion struct {
+	Nombre string
+}
+
+//Ejecucion2 struct de la tabla de ejecucion para recuperar el nombre
+type Ejecucion2 struct {
+	Condicionout string
+}
