1dd0eddf052af24810f3a17f9cf6530d51161604
jantome <59940015+jantome@users.noreply.github.com>
2020-11-12 23:29:21 +0100

    Update prueba.go

--- jantome/prueba/prueba.go
+++ jantome/prueba/prueba.go
@@ -6,7 +6,8 @@ import (
 )
 
 func main() {
-	//batch.Start("prueba")
+	/*entorno := "local"
+	batch.Start("prueba", entorno)*/
 	fmt.Println("Hola")
 	/*contador := 90
 	cabe := "\n******Estadisticas******"
@@ -15,11 +16,11 @@ func main() {
 	esta3 := "\nRegistros no tratados: 10"
 	esta4 := "\n******Fin Estadistidas******"
 	inf := cabe + esta1 + esta2 + esta3 + esta4
-	batch.Impr("prueba", inf, "w")
+	batch.Impr("prueba", inf, "w", entorno)
 	time.Sleep(60 * time.Second)
-	batch.FinOk("prueba")
+	batch.FinOk("prueba", entorno)
 	retorno := "100"
 	descripcion := "Error en bla bla lba status.."
-	batch.FinKo("prueba", retorno, descripcion)*/
+	batch.FinKo("prueba", retorno, descripcion, entorno)*/
 	os.Exit(0)
 }
