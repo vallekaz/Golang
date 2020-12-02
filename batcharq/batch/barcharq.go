f7e7cc207afa0c51a055f2c0c45c385506b7d270
jantome <59940015+jantome@users.noreply.github.com>
2020-12-02 23:51:08 +0100

    Update barcharq.go

--- batcharq/batch/barcharq.go
+++ batcharq/batch/barcharq.go
@@ -28,7 +28,7 @@ var (
 //Start funcion que se utilizara para generar el fichero de log nada más empezar un proceso batch
 func Start(nombre string, entorno string) {
 	//apertura del fichero, nos devuelve file para que podamos cerrarlo cuando finalicemos la funcion
-	file := OpenFicher(nombre, entorno)
+	file := OpenFicher(nombre, entorno, true)
 	//Comprobamos el tipo de error para ver que tenemos que guardar
 	infoLogger.Println("Start", nombre)
 	//Cerramos fichero
@@ -36,12 +36,24 @@ func Start(nombre string, entorno string) {
 }
 
 //OpenFicher funcion que se invocara en todos los sitios para la apertura del fichero
-func OpenFicher(nombre string, entorno string) (file *os.File) {
+func OpenFicher(nombre string, entorno string, primera bool) (file *os.File) {
 	//obtenemos la ruta donde se guardaran los logs
 	//pathLog := "./" + nombre + formateado
 	environment.Loadenvironment(entorno)
 	pathLog, _ := os.LookupEnv("PATH_LOG")
-	pathLog = pathLog + nombre + formateado
+	//OJO de manera temporal, vamos a tener el nombre del fichero sin año mes día. Es decir vamos a tener solo
+	//un fichero por ejecucion, hasta que tengamos un historico de ejecuciones. Por lo que el nombre por el momento
+	//sera siempre el mismo. Por lo que tenemos que hacer un delete primero
+	//pathLog = pathLog + nombre + formateado
+	pathLog = pathLog + nombre
+	//si es la primera vez borramos el fichero
+	if primera == true {
+		err := os.Remove(pathLog)
+		if os.IsNotExist(err) == false {
+			log.Fatal(err)
+		}
+	}
+	//pathLog = pathLog + nombre + formateado
 	/*pathLog, _ := os.LookupEnv("PATH_LOG")*/
 	//apertura del fichero o creacion del mismo en caso de no existir
 	file, err := os.OpenFile(pathLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
@@ -59,7 +71,7 @@ func OpenFicher(nombre string, entorno string) (file *os.File) {
 //FinOk funcion para la finalización correcta del proceso batch
 func FinOk(nombre string, entorno string) {
 	//apertura del fichero, nos devuelve file para que podamos cerrarlo cuando finalicemos la funcion
-	file := OpenFicher(nombre, entorno)
+	file := OpenFicher(nombre, entorno, false)
 	infoLogger.Println("Finish",
 		nombre,
 		"Tiempo de ejecucion: ",
@@ -70,7 +82,7 @@ func FinOk(nombre string, entorno string) {
 
 // Impr funcion que servirar para imprimir toda la info que creamos necesaria. Por ejempo estadisticas o display's
 func Impr(nombre string, inf string, tipo string, entorno string) {
-	file := OpenFicher(nombre, entorno)
+	file := OpenFicher(nombre, entorno, false)
 	switch tipo {
 	//warning
 	case "w":
@@ -90,7 +102,7 @@ func Impr(nombre string, inf string, tipo string, entorno string) {
 
 //FinKo funcion para imprimir error cuando esta KO
 func FinKo(nombre string, retorno string, descripcion string, entorno string) {
-	file := OpenFicher(nombre, entorno)
+	file := OpenFicher(nombre, entorno, false)
 	errorLogger.Println("\n¡¡¡¡¡¡Error!!!!!!\n",
 		"Retorno: ",
 		retorno,
