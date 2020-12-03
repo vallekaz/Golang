b1fa9225fadbf4a6a71f191c4fd7497280d848a2
jantome <59940015+jantome@users.noreply.github.com>
2020-12-03 21:42:45 +0100

    Update db2.go

--- jantome/apicm/db2/db2.go
+++ jantome/apicm/db2/db2.go
@@ -3,6 +3,7 @@ package db2
 import (
 	//para darle formato a la variable
 	"fmt"
+	"os"
 
 	//Librerías para base de datos
 	"database/sql"
@@ -34,10 +35,10 @@ func EjecutaQuery(query string) (result *sql.Rows, e error) {
 //ConnectDb2 FUNCION PARA LA CONEXIÓN CON LA BASE DE DATOS (Devuelve un DB y error)
 func connectDb2() (db *sql.DB, e error) {
 	//Datos para la conexión
-	usuario := "root"
-	pass := ""
-	host := "tcp(127.0.0.1:3306)"
-	nombreBaseDeDatos := "cm"
+	usuario, _ := os.LookupEnv("DB_USER")
+	pass, _ := os.LookupEnv("DB_PASSWORD")
+	host, _ := os.LookupEnv("DB_HOST")
+	nombreBaseDeDatos, _ := os.LookupEnv("DB_DATABASE")
 	//el formato tiene que ser  sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/batch")
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@%s/%s", usuario, pass, host, nombreBaseDeDatos))
 	//si es error devolvemos el error
