14963ec63a2554525acc57accfd73fdd7e6cf7b1
jantome <59940015+jantome@users.noreply.github.com>
2020-12-03 21:43:00 +0100

    Update main.go

--- jantome/apicm/main.go
+++ jantome/apicm/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/jantome/apicm/environment"
 	"github.com/jantome/apicm/routes"
+)
+
+var (
+	//recuperamos el entorno de ejecucion mediante flags para saber las rutas
+	entorno = flag.String("entorno", "", "entorno de ejecución")
 )
 
 func init() {
@@ -12,11 +21,56 @@ func init() {
 	routes.Routes()
 }
 func main() {
-	port := ":8585"
-	//Ponemos a escuchar el servidor, el nil es por que las routas ya las arrancamos
-	err := http.ListenAndServe(port, nil)
-	//controlamos el error
-	if err != nil {
-		log.Fatal(err.Error())
+	flag.Parse()
+	//fmt.Println("entorno", *entorno)
+	fmt.Println("Arrancando servidor...")
+	//Cargamos la variables de entorno
+	environment.Loadenvironment(*entorno)
+	port, _ := os.LookupEnv("SERV_PORT")
+	portSSL, _ := os.LookupEnv("SERV_PORT_SSL")
+	serSafe, _ := os.LookupEnv("SERV_SAFE")
+	switch serSafe {
+	//servidor seguro
+	case "S":
+		fmt.Println("Port safe: ", portSSL)
+		fmt.Println("Starting secure server")
+		//Arrancamos con seguridad SSL
+		err := http.ListenAndServeTLS(portSSL, "cert.pem", "key.pem", nil)
+		//controlamos el error de listener
+		if err != nil {
+			log.Fatal(err)
+		}
+	//Sin servidor seguro
+	case "N":
+		fmt.Println("Port no safe: ", port)
+		fmt.Println("Starting unsecure server")
+		//Ponemos a escuchar el servidor (Arrancamos, con las rutas ya montadas)
+		err := http.ListenAndServe(port, nil)
+		//Arracamos sin serguridad SSL
+		if err != nil {
+			log.Fatal(err)
+		}
+	//Arrancar ambos servidores seguro/no seguro
+	case "A":
+		//Arrancamos con go para primero arrancar sin seguridad y luego funcion con seguridad
+		go func() {
+			fmt.Println("Port safe: ", portSSL)
+			fmt.Println("Starting secure server")
+			//Arrancamos con seguridad SSL
+			err := http.ListenAndServeTLS(portSSL, "cert.pem", "key.pem", nil)
+			//Arracamos sin serguridad SSL
+			if err != nil {
+				log.Fatal(err)
+			}
+
+		}()
+		fmt.Println("Port no safe: ", port)
+		fmt.Println("Starting unsecure server")
+		//Ponemos a escuchar el servidor (Arrancamos, con las rutas ya montadas)
+		err := http.ListenAndServe(port, nil)
+		//Arracamos sin serguridad SSL
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
