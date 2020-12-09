7c2cb2944e4bf7b1aa156a78f8db9de17605984b
jantome <59940015+jantome@users.noreply.github.com>
2020-12-10 00:12:27 +0100

    Create environment.go

---
+++ onlinearq/online/environment/environment.go
@@ -0,0 +1,35 @@
+package environment
+
+import (
+	//Librería de sistema para cargar las variables
+
+	"log"
+	"os"
+
+	//Librería para las variables de entorno
+	"github.com/joho/godotenv-master"
+)
+
+//Loadenvironment carga de las variables de sistema
+func Loadenvironment(entorno string) {
+	//Abrimos fichero
+	//si variable entorno = local ponemos el nombre de la carpeta principal
+	if entorno == "local" {
+		err := godotenv.Load("C:\\gopath\\src\\github.com\\onlinearq\\online\\onlinearq.env")
+		//controlamos error en la apertura del fichero
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	} else {
+		// en caso de que la variable no este activada ponemos la ruta de linux
+		err := godotenv.Load("/ejecutable/online/env/onlinearq.env")
+		//controlamos error en la apertura del fichero
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
+	}
+	//******SERVIDOR******//
+	os.LookupEnv("PATH_LOG")
+	//******SERVIDOR******//
+
+}
